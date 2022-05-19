96f4db935becd88c5acebbe84858d83599ce2420
ANmishra-a <anujm2266@gmail.com>
2022-05-19 12:00:53 +0530

    introsuction to Hash table

--- day34/script.js
+++ day34/script.js
@@ -67,4 +67,142 @@
 // console.log(circular(array, 6));
 // console.log(circular(array, 7));
 // console.log(circular(array, 8));
-class array {}
+// function solution(roman) {
+//   const arrayNum = [];
+//   const slice = roman.split("");
+//   slice.forEach(function (letter) {
+//     arrayNum.push(symbol[letter]);
+//   });
+//   let j = 1;
+//   let sum = 0;
+//   for (let i = 0; i <= arrayNum.length; i + 2) {
+//     if (arrayNum[j] === undefined) {
+//       sum += arrayNum[i];
+//     } else if (arrayNum[i] >= arrayNum[j]) {
+//       sum += arrayNum[i] + arrayNum[j];
+//       j + 2;
+//       console.log(sum, j);
+//     } else if (arrayNum[i] < arrayNum[j]) {
+//       sum += arrayNum[j] - arrayNum[i];
+//       j + 2;
+//       console.log(sum, j);
+//     }
+//     console.log(symbol[roman], slice, arrayNum, sum);
+//   }
+//   return sum;
+// }
+// const symbol = {
+//   I: 1,
+//   V: 5,
+//   X: 10,
+//   L: 50,
+//   C: 100,
+//   D: 500,
+//   M: 1000,
+// };
+
+// console.log(solution("MMV"));
+// class arrays {
+//   constructor() {
+//     this.length = 0;
+//     this.data = [];
+//   }
+//   retrieve(index) {
+//     // console.log(index);
+//     return this.data[index];
+//   }
+//   push(value) {
+//     this.data[this.length - 1] = value;
+//     this.length++;
+//   }
+//   pop() {
+//     this.data.delete(this.length - 1);
+//     this.length--;
+//   }
+//   unshift(value) {
+//     for (let i = this.length; (i = 0); i--) {
+//       this.data[i] = this.data[i + 1];
+//     }
+//     this.data[0] = value;
+//   }
+// }
+// const myArray = new arrays();
+// console.log(myArray.push(12));
+// console.log(myArray);
+// console.log(myArray.retrieve(0));
+// console.log(myArray.unshift(8));
+// class HashTable {
+//   constructor(size) {
+//     this.data = new Array(size);
+//   }
+//   _hash(key) {
+//     let hash = 0;
+//     for (let i = 0; i < key.length; i++) {
+//       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+//     }
+//     return hash;
+//   }
+//   set(key, value) {
+//     let code = this._hash(key);
+//     if (!this.data[code]) {
+//       this.data[code] = [];
+//     }
+//     this.data[code].push([key, value]);
+//   }
+//   get(key) {
+//     let code = this._hash(key);
+//     const bucket = this.data[code];
+//     if (bucket) {
+//       for (let i = 0; i < bucket.length; i++) {
+//         if (bucket[i][0] === key) {
+//           return bucket[i][1];
+//         }
+//       }
+//     }
+//     return undefined;
+//   }
+//   key() {
+//     const arr = [];
+//     for (let i = 0; i < this.data.length; i++) {
+//       if (this.data[i]) {
+//         arr.push(this.data[i][0][0]);
+//       }
+//     }
+//     return arr;
+//   }
+// }
+// const myHashTable = new HashTable(10);
+// myHashTable.set("grapes", 10000);
+// myHashTable.set("gr", 10000);
+// myHashTable.set("apes", 10000);
+// myHashTable.set("grp", 1000);
+// myHashTable.set("pes", 10000);
+// console.log(myHashTable.key());
+// const firstRepeat = function (arr) {
+//   let myMap = new Map();
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!myMap.has(arr[i])) {
+//       myMap.set(arr[i], i);
+//       console.log(myMap);
+//     } else {
+//       return arr[i];
+//     }
+//   }
+//   return undefined;
+// };
+
+// const firstRepeat = function (arr) {
+//   let map = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(map);
+//     if (map[arr[i]] !== undefined) {
+//       return arr[i];
+//     } else {
+//       map[arr[i]] = i;
+//     }
+//   }
+
+//   return undefined;
+// };
+
+// console.log(firstRepeat([1, 3, 2, 1, 2, 4]));
